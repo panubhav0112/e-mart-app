9926519237a45722f36881ae2870dccdd1517c76
unknown <panubhav0112@.com>
2021-11-14 16:28:44 +0530

    made an e-commerece website and prepare for deployment

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -14,24 +14,24 @@ const protect = asyncHandler(async (req, res, next) => {
       req.user = await User.findById(decoded.id).select("-password");
       next();
     } catch (error) {
-      res.status(401)
-      throw new Error('Not authorized , token failed')
+      res.status(401);
+      throw new Error("Not authorized , token failed");
     }
   }
   if (!token) {
     res.status(401);
     throw new Error("Unauthorized !");
   }
-//   next();
+  //   next();
 });
 
 const admin = (req, res, next) => {
-  if(req.user && req.user.isAdmin){
-    next()
+  if (req.user && req.user.isAdmin) {
+    next();
   } else {
-    res.status(401)
-    throw new Error('Not authorized as an admin')
+    res.status(401);
+    throw new Error("Not authorized as an admin");
   }
-}
+};
 
 export { protect, admin };
--- backend/routes/OrderRoutes.js
+++ backend/routes/OrderRoutes.js
@@ -1,6 +1,13 @@
 import express from 'express'
 const router = express.Router()
-import { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders, getOrders, updateOrderToDelivered } from '../controllers/OrderControllers.js'
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getMyOrders,
+  getOrders,
+} from '../controllers/OrderControllers.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders)
