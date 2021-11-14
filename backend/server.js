5aa847e534fd478c88749e846de59fc120701981
unknown <panubhav0112@.com>
2021-11-14 16:37:44 +0530

    made an e-commerece website and prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import path from "path";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-import orderRoutes from "./routes/orderRoutes.js";
+import orderRoutes from "./routes/OrderRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import morgan from "morgan";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -21,7 +21,7 @@ app.use(express.json());
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
-app.use("/api/orders", OrderRoutes);
+app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 
 app.get("/api/config/paypal", (req, res) =>
